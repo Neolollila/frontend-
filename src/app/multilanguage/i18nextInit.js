27ab8778e98759a84c928f5279a97b2cadccff12
JeZ <JeZ@DESKTOP-MEITL6E>
2021-08-29 17:56:01 +0300

    don't change languages.... heroku....

--- src/app/multilanguage/i18nextInit.js
+++ src/app/multilanguage/i18nextInit.js
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-xhr-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
+// import LanguageDetector from "i18next-browser-languagedetector";
 import translationEN from "../public/assets/locales/en/translation.json";
 import translationRU from "../public/assets/locales/ru/translation.json";
 
@@ -20,7 +20,7 @@ const resources = {
 
 i18n
     .use(Backend)
-    .use(LanguageDetector)
+    // .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources,
